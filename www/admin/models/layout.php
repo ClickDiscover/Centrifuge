8e105601195e3e2864efc0fd04b82e42b408069a
Patrick Madden <maddenpj@gmail.com>
2015-08-07 05:03:31 -0500

    Add Menu to admin interface

--- www/admin/models/layout.php
+++ www/admin/models/layout.php
@@ -1,12 +1,32 @@
 <html>
 <head>
-<title><?= $this->e($title) ?></title>
-<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
-<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+    <title><?= $this->e($title) ?></title>
+    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+    <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+
+    <style>
+        #page {
+          margin-left: 50px;
+          margin-right: 50px;
+        }
+
+        #menu-container {
+            display: inline-block;
+            height: 33px;
+            padding-bottom: 20px;
+        }
+    </style>
+
 </head>
 <body>
 
-<?=$this->section('page')?>
+<div id="menu-container">
+    <?= $this->fetch('admin::models/menu') ?>
+</div>
+
+<div id="page">
+    <?=$this->section('page')?>
+</div>
 
 </body>
 </html>
---
+++ www/admin/models/menu.php
@@ -0,0 +1,12 @@
+
+<div class="menu">
+    <?php $v = '<a href="/"><img src="" class="logo"></a>' ?>
+    <div class="pure-menu pure-menu-horizontal">
+        <span class="pure-menu-heading">CENTRIFUGE</span>
+        <ul class="pure-menu-list">
+            <li class="pure-menu-item"><a href="/admin/models" class="pure-menu-link">Models</a></li>
+            <li class="pure-menu-item"><a href="/admin/models/landers" class="pure-menu-link">Landers</a></li>
+            <li class="pure-menu-item"><a href="/admin/models/products" class="pure-menu-link">Products</a></li>
+        </ul>
+    </div>
+</div>
