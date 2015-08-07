a5ff283a79ec6adb6a4612e6f00e0fba5c85bd72
Patrick Madden <maddenpj@gmail.com>
2015-08-07 02:55:55 -0500

    Refactor admin routes

--- app/routes/admin.php
+++ app/routes/admin.php
@@ -4,6 +4,20 @@ require CENTRIFUGE_ROOT . '/vendor/autoload.php';
 require_once CENTRIFUGE_MODELS_ROOT . "/product.php";
 
 
+function cleanAeParams($aeParams) {
+    $clean = array();
+    foreach ($aeParams as $i => $ae) {
+        $clean[$i] = $ae;
+        if(!isset($ae['name'])) {
+            $clean[$i]['name'] = implode(' | ', array(
+                $ae['affiliate_id'],
+                $ae['country'],
+                $ae['vertical']
+            ));
+        }
+    }
+    return $clean;
+}
 
 
 $app->path('admin', function($req) use ($app) {
@@ -14,62 +28,52 @@ $app->path('admin', function($req) use ($app) {
         return "pong!";
     });
 
-    $app->path('products', function() use ($app) {
-        $app->get(function () use ($app) {
-            $results = $app->db()->query('SELECT * FROM products')->fetchAll(PDO::FETCH_ASSOC);
-            return $app->plates->render('admin::products', array(
-                'products' => $results
-            ));
-        });
-    });
+    $app->path('models', function() use ($app) {
+        $websites = $app->db()->query('SELECT * FROM websites')->fetchAll(PDO::FETCH_ASSOC);
+        $products = $app->db()->query('SELECT * FROM products')->fetchAll(PDO::FETCH_ASSOC);
+        $routes   = $app->db()->query('SELECT * FROM routes')->fetchAll(PDO::FETCH_ASSOC);
+        $landers  = $app->db()->query('SELECT * FROM landers')->fetchAll(PDO::FETCH_ASSOC);
+        $aeParams = cleanAEParams($app->db()->query('SELECT * FROM ae_parameters')->fetchAll(PDO::FETCH_ASSOC));
+        $allModels = array(
+            "websites" => $websites,
+            "products" => $products,
+            "routes" => $routes,
+            "aeParams" => $aeParams,
+            "landers" => $landers
+        );
 
-    $app->path('landers', function ($req) use ($app) {
-        $app->param('int', function ($req, $id) use ($app) {
-            $lander = LanderFunctions::fetch($app, $id);
-            $app->get(function () use ($app, $lander)  {
-                return $app->plates->render('admin::testing/base', $lander->toArray());
+
+        $app->path('products', function() use ($app) {
+            $app->get(function () use ($app) {
+                $results = $app->db()->query('SELECT * FROM products')->fetchAll(PDO::FETCH_ASSOC);
+                return $app->plates->render('admin::products', array(
+                    'products' => $results
+                ));
             });
         });
-    });
 
+        $app->path('landers', function ($req) use ($app) {
+            $app->param('int', function ($req, $id) use ($app) {
+                $lander = LanderFunctions::fetch($app, $id);
+                $app->get(function () use ($app, $lander)  {
+                    return $app->plates->render('admin::testing/base', $lander->toArray());
+                });
+            });
 
-    $app->path('create', function ($req) use ($app) {
-        $app->get(function () use ($app) {
-            $websites = $app->db()->query('SELECT * FROM websites')->fetchAll(PDO::FETCH_ASSOC);;
-            $products = $app->db()->query('SELECT * FROM products')->fetchAll(PDO::FETCH_ASSOC);;
-            $routes   = $app->db()->query('SELECT * FROM routes')->fetchAll(PDO::FETCH_ASSOC);;
-            $landers  = $app->db()->query('SELECT * FROM landers')->fetchAll(PDO::FETCH_ASSOC);;
-            $aeParams = $app->db()->query('SELECT * FROM ae_parameters')->fetchAll(PDO::FETCH_ASSOC);;
-
-            foreach ($aeParams as $i => $ae) {
-                if(!isset($ae['name'])) {
-                    $aeParams[$i]['name'] = implode(' | ', array(
-                        $ae['affiliate_id'],
-                        $ae['country'],
-                        $ae['vertical']
-                    ));
-                }
-            }
-
-            return $app->plates->render('admin::landers', array(
-                "websites" => $websites,
-                "products" => $products,
-                "routes" => $routes,
-                "aeParams" => $aeParams,
-                "landers" => $landers
-            ));
+            $app->post(function ($req) use ($app) {
+                $result = LanderFunctions::insert($app, $req->post());
+                $out = "<pre>";
+                $out .= "LanderFunctions::insert" . PHP_EOL;
+                $out .= print_r($result, true);
+                $out .= "</pre>";
+                return $out;
+            });
         });
 
-        $app->post(function ($req) use ($app) {
-            $result = LanderFunctions::insert($app, $req->post());
-            $out = "<pre>";
-            $out .= "LanderFunctions::insert" . PHP_EOL;
-            $out .= print_r($result, true);
-            $out .= "</pre>";
-            return $out;
+
+        $app->get(function () use ($app, $allModels) {
+            return $app->plates->render('admin::landers', $allModels);
         });
     });
-
-
 });
 
