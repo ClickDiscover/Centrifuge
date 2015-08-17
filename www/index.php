83c81745a3a0fc5fed376694eb7faa9261b69d3c
Patrick Madden <maddenpj@gmail.com>
2015-08-17 07:34:50 -0500

    PHP Cli server bullshit with routing

--- www/index.php
+++ www/index.php
@@ -1,8 +1,11 @@
 <?php
 if(php_sapi_name() === 'cli-server') {
-    $filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
-    if(is_file($filename)) {
+    if(preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER['REQUEST_URI'])) {
         return false;
+    }
+    if (strpos($_SERVER['PHP_SELF'], '/index.php') === false) {
+        $_SERVER['PHP_SELF'] = '/index.php' . $_SERVER['PHP_SELF'];
+        $_SERVER['SCRIPT_NAME'] = '/index.php';
     }
 }
 
@@ -10,21 +13,19 @@ require_once dirname(__DIR__) . '/config.php';
 require CENTRIFUGE_ROOT . '/vendor/autoload.php';
 require CENTRIFUGE_APP_ROOT . '/centrifuge.php';
 
-$centrifuge =  new Centrifuge($config);
+$centrifuge =  new SlimBootstrap($config);
 $app = new Slim\Slim($config);
-$app->setName(APPLICATION_NAME);
-$centrifuge->instrumentSlim($app);
-
-
+$app->setName($config['name']);
+// $centrifuge->instrumentSlim($app);
 
 $app->get('/hello/:name', function ($name) use ($app) {
-    $app->log->info("Testing");
-    $sites = $app->container['db']->query("SELECT distinct namespace from websites")->fetchAll(PDO::FETCH_ASSOC);
+    $sites = $app->container['db']->query("SELECT distinct namespace from websites")->fetchAll();
     $app->render('admin::models/test', array('sites' => $sites, 'name' => $name));
 });
 
-require_once $config['paths']['routes'] . 'conversions.php';
+require_once $config['paths']['routes'] . 'landers.php';
 require_once $config['paths']['routes'] . 'clicks.php';
+require_once $config['paths']['routes'] . 'conversions.php';
 
 ?><pre><?php
 $app->run();
