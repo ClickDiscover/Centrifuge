fefd86adeb60de8c78392cda6c0fcf7b7d0cfb6e
Patrick Madden <maddenpj@gmail.com>
2015-09-24 16:33:10 -0500

    Function queue now has direct integration with profiler

--- src/Flagship/Util/FunctionQueueTrait.php
+++ src/Flagship/Util/FunctionQueueTrait.php
@@ -2,18 +2,29 @@
 
 namespace Flagship\Util;
 
+use Flagship\Util\Profiler\Profiling;
 
 trait FunctionQueueTrait {
 
+    use Profiling;
+
     public $_funcs = [];
 
-    public function enqueue(\Closure $callback) {
-        $this->_funcs[] = $callback;
+    public function enqueue(\Closure $callback, $namespace = null, $key = null) {
+        $this->_funcs[] = [
+            'cb' => $callback,
+            'label' => $this->_key($namespace, $key)
+        ];
     }
+
     public function flushQueue() {
         $res = [];
-        foreach ($this->_funcs as $cb) {
+        foreach ($this->_funcs as $closure) {
+            $label = $closure['label'];
+            $cb = $closure['cb'];
+            $this->getProfiler()->start($label);
             $res[] = $cb();
+            $this->getProfiler()->stop($label);
         }
         return $res;
     }
