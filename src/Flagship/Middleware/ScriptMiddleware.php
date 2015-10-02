166db9abac270e1cc2aadca489fe711d22a343dc
Patrick Madden <maddenpj@gmail.com>
2015-10-01 23:34:31 -0500

    Modify script middleware to insert piwik and countly tags

--- src/Flagship/Middleware/ScriptMiddleware.php
+++ src/Flagship/Middleware/ScriptMiddleware.php
@@ -16,9 +16,17 @@ class ScriptMiddleware extends Middleware {
 
     protected $scripts = [];
 
-    public function __construct(Container $container) {
-        $this->setProfiler($container['profiler']);
-        $this->setProfilingClass('ScriptMiddleware');
+    protected $siteDomain;
+    protected $piwikHost;
+    protected $countlyHost;
+
+    protected $insertTag = '</body>';
+
+    public function __construct() {
+        // $this->setProfiler($container['profiler']);
+        // $this->setProfilingClass('ScriptMiddleware');
+        $this->piwikHost = '52.26.98.211';
+        $this->addScript($this->piwik());
     }
 
     public function addScript($s) {
@@ -40,7 +48,7 @@ class ScriptMiddleware extends Middleware {
     }
 
     protected function insertScriptTags($html) {
-        $pos = mb_strripos($html, '</body>');
+        $pos = mb_strripos($html, $this->insertTag);
         $tags = $this->buildScriptTags();
         if ($pos === false) {
             $html .= $tags;
@@ -51,7 +59,53 @@ class ScriptMiddleware extends Middleware {
     }
 
     protected function buildScriptTags() {
-        return implode('\n', $this->scripts);
+        return implode("\n", $this->scripts);
+    }
+
+    public function countly() {
+        $countly = <<<HTML
+<script type="text/javascript" src="/static/admin/countly.js"></script>
+<script type="text/javascript">
+Countly.init({
+    app_key: "560c189025e4082b0029df50",
+    url: "http://192.168.99.100:32780", //or none for default countly cloud
+    debug:true
+})
+console.log("Countly");
+Countly.begin_session();
+//add any events you want like pageView
+// Countly.add_event({
+    // "key": "pageView",
+        // "segmentation": {
+        // "url": window.location.pathname
+    // }
+// });
+Countly.track_pageview();
+window.onunload = function(){ Countly.end_session(); };
+</script>
+HTML;
+        return $countly;
+    }
+
+    public function piwik() {
+
+        $host = $this->piwikHost;
+        $piwik = <<<HTML
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//$host/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 1]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//$host/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+HTML;
+        return $piwik;
     }
 
 }
